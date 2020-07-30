604893c9fb91c5ae5895fcd6a2b2f80ade6cbbc6
SimplyDifficult <47187515+SimplyDifficult@users.noreply.github.com>
2020-07-30 12:03:47 +0530

    Create *B-Longest Palindrome.cpp

---
+++ Strings/*B-Longest Palindrome.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fo(i,n) for(int i=0;i<n;i++)
+#define fo1(i,n) for(int i=1;i<=n;i++)
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define int long long
+const int N=2e5+1;
+int mod=1000000007;
+
+
+int power(int a,int b) {int ans=1; while(b){if(b&1)ans=(ans*a)%mod;b/=2;a=(a*a)%mod;}return ans;}
+int bit(int n){if(n==0)return 0;int k=1,b=0;while((n/k)!=0){k*=2;b++;}return b-1;}
+
+int n,t,k,m,x,y,flag=1;
+string s[101];
+
+
+void solve(){
+    cin>>n>>m;
+    set<string>dict;
+    fo(i,n){
+    	cin>>s[i];
+    	dict.insert(s[i]);
+    }
+    vector<string>left,right;
+    string mid;
+    fo(i,n){
+    	string t=s[i];
+    	reverse(t.begin(),t.end());
+    	if(t==s[i])mid=t;
+    	else if(dict.find(t)!=dict.end()){
+    		left.pb(s[i]);
+    		right.pb(t);
+    		dict.erase(s[i]);
+    		dict.erase(t);
+    	}
+    }
+    cout<<left.size()*m*2+mid.size()<<endl;
+    for(string x:left)cout<<x;
+   	cout<<mid;
+    reverse(right.begin(),right.end());
+    for(string x:right)cout<<x;
+    cout<<endl;
+}
+
+int32_t main(){
+
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+// 	cin>>t;
+// 	while(t--){
+ 		solve();
+// 	}
+
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+    return 0;
+} 
